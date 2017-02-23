f1f0936a0472ecdc92f9a50d57c9bc4dd1f6cd0d
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-23 09:47:51 +0100

    Made skeleton for the service

---
+++ src/main/java/com/cegekaschool/domain/customer/CustomerService.java
@@ -0,0 +1,12 @@
+package com.cegekaschool.domain.customer;
+
+/**
+ * Created by roelg on 23/02/2017.
+ */
+public class CustomerService {
+    public void addCustomer(Customer customer) {
+    }
+
+    public Iterable<Customer> getAllCustomers() {
+    }
+}
