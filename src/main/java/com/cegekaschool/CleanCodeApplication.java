4aa3ae2ab0e986e58473e3f85817f812cfeeeaa0
Roel Goossens <roelgoossens22@hotmail.com>
2017-02-23 09:47:11 +0100

    Gonna use spring yo

---
+++ src/main/java/com/cegekaschool/CleanCodeApplication.java
@@ -0,0 +1,12 @@
+package com.cegekaschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeApplication.class, args);
+    }
+}
